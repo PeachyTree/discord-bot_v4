4105b66ec8ab55f072a4b62ccb4d3a86cf0ac4c5
TrueXRetro <dragonminner867@outlook.com>
2019-02-13 20:45:36 +0100

    Small change

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,5 @@
-// The command will be activated using -ping, since the filename is ping & our prefix is -
-
 exports.run = (client, message, args) => {
 
-    // This sends a message to the channel, containing the string 'Pong!'
     message.channel.send('Pong!');
 
-} // Now, let's test it out! - To start the bot type node index.js in terminal, & don't forget to save the file.
+} 
