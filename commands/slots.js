dfc865a6f4e94a1b9b5c7de0d286f11628cc76fe
mlp_Pinkie_Pie <dragonminner867@outlook.com>
2019-07-12 08:28:29 +0200

    Updated embed

--- commands/slots.js
+++ commands/slots.js
@@ -1,32 +1,30 @@
 // 🍎🍌🍒🍓🍈
 
-const Discord = require('discord.js'); 
+const {RichEmbed} = require('discord.js'); 
 
-// Command Handler
 exports.run = (client, message, args) => {
 
     let slots = ["🍎", "🍌", "🍒", "🍓", "🍈"];
     let result1 = Math.floor((Math.random() * slots.length));
     let result2 = Math.floor((Math.random() * slots.length));
     let result3 = Math.floor((Math.random() * slots.length));
-    let name = message.author.displayName;
-    let icon = message.author.displayAvatarURL;
 
     if (slots[result1] === slots[result2] && slots[result3]) {
-        let wEmbed = new Discord.RichEmbed() // Remember to use MessageEmbed if you use master
-            .setFooter('You won!', icon)
+        
+        let embed = new RichEmbed() 
+            .setFooter('You won!', message.author.displayAvatarURL)
             .setTitle(':slot_machine: Slots :slot_machine:')
             .addField('Result:', slots[result1] + slots[result2] + slots[result3], true)
             .setColor(0xF4E842)
-        message.channel.send(wEmbed);
+        message.channel.send({embed});
 
     } else {
 
-        let lEmbed = new Discord.RichEmbed()
-            .setFooter('You lost!', icon)
+        let embed = new RichEmbed()
+            .setFooter('You lost!', message.author.displayAvatarURL)
             .setTitle(':slot_machine: Slots :slot_machine:')
             .addField('Result:', slots[result1] + slots[result2] + slots[result3], true)
             .setColor(0xF4E842)
-        message.channel.send(lEmbed);
+        message.channel.send({embed});
     }
 } 
