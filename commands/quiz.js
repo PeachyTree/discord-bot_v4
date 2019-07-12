a65ce2af46b7db21d877d99a180ca38b1ca5d208
mlp_Pinkie_Pie <dragonminner867@outlook.com>
2019-07-12 08:20:47 +0200

    Updated Embeds

--- commands/quiz.js
+++ commands/quiz.js
@@ -1,16 +1,15 @@
-const Discord = require('discord.js');
+const {RichEmbed} = require('discord.js');
 
-// Command Handler
 exports.run = async (client, message, args) => {
 
   let quiz = [
     { q: 'What color is the sky?', a: ['no color', 'invisible'] },
     { q: 'Name a soft drink brand.', a: ['pepsi', 'coke', 'rc', '7up', 'sprite', 'mountain dew'] },
     { q: 'Name a programming language.', a: ['actionscript', 'coffescript', 'c', 'c++', 'c#', 'visual basic', 'perl', 'javascript', 'dotnet', 'crystal', 'lua', 'go', 'python', 'rust', 'ruby', 'java'] },
-  ];
+  ]; // You can of course add more questions and answers. There is no limit!
   let options = {
     max: 1,
-    time: 30050, // 30050 = 30 seconds and a half ms
+    time: 30050, // 30050 = 30 seconds and a half ms, you can change this if you'd like.
     errors: ['time'],
   };
 
@@ -19,18 +18,20 @@ exports.run = async (client, message, args) => {
   try {
     let collected = await message.channel.awaitMessages(answer => item.a.includes(answer.content.toLowerCase()), options);
     let winnerMessage = collected.first();
-    return message.channel.send({embed: new Discord.RichEmbed()
+    
+    return message.channel.send({embed: new RichEmbed()
                                   .setAuthor(`Winner: ${winnerMessage.author.tag}`, winnerMessage.author.displayAvatarURL)
                                   .setTitle(`Correct Answer: \`${winnerMessage.content}\``)
                                   .setFooter(`Question: ${item.q}`)
-                                  .setColor('RANDOM')
+                                  .setColor('RANDOM') // Change this if you want to.
                                 })
 
   } catch (_) {
-    return message.channel.send({embed: new Discord.RichEmbed()
+    return message.channel.send({embed: new RichEmbed()
                                   .setAuthor('No one got the answer in time!')
                                   .setTitle(`Correct Answer(s): \`${item.a}\``)
                                   .setFooter(`Question: ${item.q}`)
+                                  .setColor('RANDOM') // Change this if you want to.
                                 })
 
   }
